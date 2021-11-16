da2ca19c0ac48093788a62416388ced95e4e1f5a
Brandsma Siringoringo <brandsmasiringo123@gmail.com>
2021-11-16 11:24:51 +0700

    [Brandsma] Succesfull to make a method delete transaction

--- src/main/java/com/example/future_bank/controler/TransactionControler.java
+++ src/main/java/com/example/future_bank/controler/TransactionControler.java
@@ -3,10 +3,7 @@ package com.example.future_bank.controler;
 import com.example.future_bank.entity.Transaction;
 import com.example.future_bank.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 import java.util.Date;
@@ -27,5 +24,10 @@ public class TransactionControler {
     @GetMapping("/transactions")
     public List<Transaction> getAllTransaction(){
         return transactionService.getAllTransaction();
+    }
+
+    @DeleteMapping("/transaction")
+    public void deleteTRansactionById(@RequestParam(name = "id") String id){
+        transactionService.deleteTransaction(id);
     }
 }
