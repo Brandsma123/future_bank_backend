50aac638f5a1962e2de120478c974e8b8286fac9
Brandsma Siringoringo <brandsmasiringo123@gmail.com>
2021-11-16 09:16:23 +0700

    [Brandsma] Succesfull to make a method delete wallet by id

--- src/main/java/com/example/future_bank/controler/WalletControler.java
+++ src/main/java/com/example/future_bank/controler/WalletControler.java
@@ -4,10 +4,7 @@ import com.example.future_bank.entity.Wallet;
 import com.example.future_bank.service.WalletService;
 import com.example.future_bank.service.WalletServiceDblmpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +22,10 @@ public class WalletControler {
     @GetMapping("/wallets")
     public List<Wallet> getAllWallet(){
         return walletService.getAllWallet();
+    }
+
+    @DeleteMapping("/wallet")
+    public void deleteWalletById(@RequestParam(name = "id") String id){
+        walletService.deleteWallet(id);
     }
 }
